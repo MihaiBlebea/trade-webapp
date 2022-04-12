a2c488fafc74caa5be1800b9806e75f717302512
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-12 21:52:51 +0100

    Changes the color to dark mode

--- src/components/FixedFooter.vue
+++ src/components/FixedFooter.vue
@@ -4,11 +4,11 @@
 			<div class="row justify-content-center mb-2">
 				<div class="col-12 col-md-6 row">
 					<div class="col d-flex justify-content-evenly">
-						<router-link to="/">Home</router-link>
-						<router-link to="/discovery">Discovery</router-link>
-						<router-link to="/orders">Orders</router-link>
-						<router-link to="/portfolio">Portfolio</router-link>
-						<router-link to="/account">Account</router-link>
+						<router-link to="/"><i class="fa fa-home"></i></router-link>
+						<router-link to="/discovery"><i class="fa fa-globe"></i></router-link>
+						<router-link to="/orders"><i class="fa fa-money"></i></router-link>
+						<router-link to="/portfolio"><i class="fa fa-suitcase"></i></router-link>
+						<router-link to="/account"><i class="fa fa-cogs"></i></router-link>
 					</div>
 				</div>
 			</div>
@@ -23,5 +23,11 @@
 	width: 100%;
 	height: 60px;
 	line-height: 60px;
+}
+
+i {
+	font-size: 28px;
+	vertical-align: middle;
+	color: white;
 }
 </style>
